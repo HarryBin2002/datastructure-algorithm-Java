9138ea877479c5f0a764dcfc769ae1aadc902a04
harrybin <hoangmanhbinh2002@gmail.com>
2021-10-10 23:15:08 +0700

    update2 10/10/2021

---
+++ src/Week 5 Stack and Queue/Building by Linked/MyMain.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import week5.InterfaceStackAndQueue;
+import week5.Node;
+import week5.MyLinkedStack;
+
+public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        MyLinkedStack sk = new MyLinkedStack();
+        Node head = sk.create(x);
+        for (int i = 1; i < n; i++) {
+            x = sc.nextInt();
+            head = sk.push(head, x);
+        }
+
+        sk.show(head);
+        sk.pop(head);
+        System.out.println();
+        sk.show(head);
+        sk.pop(head);
+
+        sc.close();
+    }
+}
